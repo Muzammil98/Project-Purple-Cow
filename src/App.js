23aa25c187be0416e36ac2fc0d6207f04db2fc80
Muzammil98 <muzammilsarwar0@gmail.com>
2022-04-23 14:57:18 +0500

    added hit button

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import countapi from "countapi-js";
 import logo from "./assets/code-venture-icon.svg";
 import "./App.css";
 
+const NAMESPACE = "codeventure";
+const KEY = "1ccb732e-b55a-4404-ad3f-0f99c02fe44e";
+
 function App() {
   const [currentCount, setcurrentCount] = useState(0);
 
@@ -14,12 +17,17 @@ function App() {
   }, []);
 
   const getCurrentCount = async () => {
-    await countapi
-      .get("codeventure", "1ccb732e-b55a-4404-ad3f-0f99c02fe44e")
-      .then((result) => {
-        // console.log("COUNT", count);
-        setcurrentCount(result.value.toString());
-      });
+    await countapi.get(NAMESPACE, KEY).then((result) => {
+      // console.log("COUNT", count);
+      setcurrentCount(result.value.toString());
+    });
+  };
+
+  const hitCounter = async () => {
+    await countapi.hit(NAMESPACE, KEY).then((result) => {
+      // console.log("hitCounter", result);
+      setcurrentCount(result.value.toString());
+    });
   };
 
   return (
@@ -27,17 +35,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {currentCount}
-          Edit <code>src/App.js</code> and save to reload.
+          Count : <code>{currentCount}</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <button onClick={() => hitCounter()} className="App-link">
           Learn React
-        </a>
+        </button>
       </header>
     </div>
   );
